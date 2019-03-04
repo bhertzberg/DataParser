2cc262730f2d48926fa5b0078cbd597fc7eee937
Ben Hertzberg <hertzberg.benjamin@gmail.com>
2019-03-03 18:41:16 -0800

    add parse2016ElectionResults psuedo code

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -19,4 +20,17 @@ public class Utils {
 
         return output.toString();
     }
+
+    public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
+    /*    Create your return arraylist
+        split input data by \n to create array of rows
+        loop over all rows BUT SKIP THE FIRST ROW (look at the file to see why)
+        for each row {
+            split it into individual values and save into the right kinds of variables.
+                    Create a new object using those values
+            Add it to your list.
+        }
+    */    return results;
+    }
+
 }
