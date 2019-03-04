beb0fdd5bfada4f3f08588c3acebc9b72b21668b
Ben Hertzberg <hertzberg.benjamin@gmail.com>
2019-03-03 18:32:10 -0800

    create fields

--- src/ElectionResult.java
+++ src/ElectionResult.java
@@ -1,7 +1,18 @@
 public class ElectionResult {
+    double demVotes, gopVotes, totalVotes, demPercent, gopPercent, voteDifference, percentDifference;
+    String stateAbbreviation, county, combinedFips;
 
-    public ElectionResult(double demVotes,double gopVotes, double totalVotes, double demPercent, double gopPercent, double voteDifference, double percentDifference, String stateAbrv, String county,String combinedFips){
 
+    public ElectionResult(double demVotes, double gopVotes, double totalVotes, double demPercent, double gopPercent, double voteDifference, double percentDifference, String stateAbbreviation, String county, String combinedFips) {
+        this.demVotes = demVotes;
+        this.gopVotes = gopVotes;
+        this.totalVotes = totalVotes;
+        this.demPercent = demPercent;
+        this.gopPercent = gopPercent;
+        this.voteDifference = voteDifference;
+        this.percentDifference = percentDifference;
+        this.stateAbbreviation = stateAbbreviation;
+        this.county = county;
+        this.combinedFips = combinedFips;
     }
-
 }
