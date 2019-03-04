6f7916d4dbce6e379186564c81dc0f110de14d99
Ben Hertzberg <hertzberg.benjamin@gmail.com>
2019-03-03 18:29:21 -0800

    add constructor

--- src/ElectionResult.java
+++ src/ElectionResult.java
@@ -1,4 +1,7 @@
 public class ElectionResult {
 
+    public ElectionResult(double demVotes,double gopVotes, double totalVotes, double demPercent, double gopPercent, double voteDifference, double percentDifference, String stateAbrv, String county,String combinedFips){
+
+    }
 
 }
