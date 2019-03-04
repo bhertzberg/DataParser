cdd629960b1b7419cc53b82740e123645e27db1b
Ben Hertzberg <hertzberg.benjamin@gmail.com>
2019-03-03 18:57:08 -0800

    add parse2016ElectionResults real code

--- src/Utils.java
+++ src/Utils.java
@@ -22,15 +22,32 @@ public class Utils {
     }
 
     public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
-    /*    Create your return arraylist
-        split input data by \n to create array of rows
-        loop over all rows BUT SKIP THE FIRST ROW (look at the file to see why)
-        for each row {
-            split it into individual values and save into the right kinds of variables.
-                    Create a new object using those values
-            Add it to your list.
+     //   Create your return arraylist;
+        ArrayList<ElectionResult> results = new ArrayList<>();
+      //  split input data by \n to create array of rows
+        String[] lines = data.split("\n");
+
+     //   loop over all rows BUT SKIP THE FIRST ROW (look at the file to see why)
+        for (int a = 1; a < lines.length; a++) {
+
+     //       for each row {
+     //       split it into individual values and save into the right kinds of variables.
+            String[] lineOfCoordinates = lines[a].split(",");
+            double[] numberCoordinates = new double[lineOfCoordinates.length-3];
+            for (int i = 0; i < lineOfCoordinates.length; i++) {
+                lineOfCoordinates[i].trim();
+            }
+            for (int i = 0; i < numberCoordinates.length; i++) {
+                // make every number value ready to parse into an integer or a double by removing % or ,
+                cleanUp(lineOfCoordinates[i]);
+                numberCoordinates[i] = Double.parseDouble(lineOfCoordinates[i]);
+            }
+       //             Create a new object using those values
+            ElectionResult newElecResult = new ElectionResult(numberCoordinates[0], numberCoordinates[1], numberCoordinates[2], numberCoordinates[3], numberCoordinates[4], numberCoordinates[5], numberCoordinates[6], lineOfCoordinates[7], lineOfCoordinates[8], lineOfCoordinates[9]);
+            //     Add it to your list.
+            results.add(newElecResult);
         }
-    */    return results;
+        return results;
     }
 
 }
