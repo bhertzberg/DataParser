020f2bd4a0a13a22e249f2a8d741c3301e15e734
Ben Hertzberg <hertzberg.benjamin@gmail.com>
2019-03-03 18:32:55 -0800

    add getters and setters

--- src/ElectionResult.java
+++ src/ElectionResult.java
@@ -15,4 +15,84 @@ public class ElectionResult {
         this.county = county;
         this.combinedFips = combinedFips;
     }
+
+    public double getDemVotes() {
+        return demVotes;
+    }
+
+    public void setDemVotes(double demVotes) {
+        this.demVotes = demVotes;
+    }
+
+    public double getGopVotes() {
+        return gopVotes;
+    }
+
+    public void setGopVotes(double gopVotes) {
+        this.gopVotes = gopVotes;
+    }
+
+    public double getTotalVotes() {
+        return totalVotes;
+    }
+
+    public void setTotalVotes(double totalVotes) {
+        this.totalVotes = totalVotes;
+    }
+
+    public double getDemPercent() {
+        return demPercent;
+    }
+
+    public void setDemPercent(double demPercent) {
+        this.demPercent = demPercent;
+    }
+
+    public double getGopPercent() {
+        return gopPercent;
+    }
+
+    public void setGopPercent(double gopPercent) {
+        this.gopPercent = gopPercent;
+    }
+
+    public double getVoteDifference() {
+        return voteDifference;
+    }
+
+    public void setVoteDifference(double voteDifference) {
+        this.voteDifference = voteDifference;
+    }
+
+    public double getPercentDifference() {
+        return percentDifference;
+    }
+
+    public void setPercentDifference(double percentDifference) {
+        this.percentDifference = percentDifference;
+    }
+
+    public String getStateAbbreviation() {
+        return stateAbbreviation;
+    }
+
+    public void setStateAbbreviation(String stateAbbreviation) {
+        this.stateAbbreviation = stateAbbreviation;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getCombinedFips() {
+        return combinedFips;
+    }
+
+    public void setCombinedFips(String combinedFips) {
+        this.combinedFips = combinedFips;
+    }
 }
