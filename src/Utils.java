28dd01e0b5ce8afee24682ce010ff6c5a9317285
Ben Hertzberg <hertzberg.benjamin@gmail.com>
2019-03-03 22:27:20 -0800

    change array of all data values to an array list to account for discrpency in the length of the array of values taken from the data

--- src/Utils.java
+++ src/Utils.java
@@ -33,28 +33,30 @@ public class Utils {
      //       for each row {
      //       split it into individual values and save into the right kinds of variables.
             String[] lineOfCoordinates = lines[a].split(",");
+            ArrayList<String> coordinates = new ArrayList<>();
+            for (int i = 1; i < lineOfCoordinates.length; i++) {
+                coordinates.add(lineOfCoordinates[i]);
+            }
 
      //account for comma in difference which incorrectly splits number
             ArrayList<Double> numberCoordinates = new ArrayList<>();
             if(lineOfCoordinates.length>11) {
-                lineOfCoordinates[6] += lineOfCoordinates[7];
-                for (int i = 7; i < lineOfCoordinates.length - 1; i++) {
-                    lineOfCoordinates[i] = lineOfCoordinates[i + 1];
-                }
+               coordinates.set(5, coordinates.get(5)+ coordinates.get(6)) ;
+               coordinates.remove(6);
             }
 
-            for (int i = 1; i < lineOfCoordinates.length; i++) {
-                lineOfCoordinates[i].trim();
+            for (int i = 0; i < coordinates.size(); i++) {
+                coordinates.get(i).trim();
             }
-            for (int i = 1; i < 8; i++) {
+            for (int i = 0; i < 7; i++) {
                 // make every number value ready to parse into an integer or a double by removing % or ,
-                lineOfCoordinates[i] = cleanUp(lineOfCoordinates[i]);
-                numberCoordinates.add(Double.parseDouble(lineOfCoordinates[i]));
+                coordinates.set(i,cleanUp(coordinates.get(i)));
+                numberCoordinates.add(Double.parseDouble(coordinates.get(i)));
+
             }
        //             Create a new object using those values
             // inputs to constructor start at 1 to ignore the row number in the data
-            System.out.println(numberCoordinates.size());
-            ElectionResult newElecResult = new ElectionResult(numberCoordinates.get(1), numberCoordinates.get(2), numberCoordinates.get(3), numberCoordinates.get(4), numberCoordinates.get(5), numberCoordinates.get(6), numberCoordinates.get(7), lineOfCoordinates[8], lineOfCoordinates[9], lineOfCoordinates[10]);
+            ElectionResult newElecResult = new ElectionResult( numberCoordinates.get(0), numberCoordinates.get(1), numberCoordinates.get(2), numberCoordinates.get(3), numberCoordinates.get(4), numberCoordinates.get(5), numberCoordinates.get(6), coordinates.get(7), coordinates.get(8), coordinates.get(9));
             //     Add it to your list.
             results.add(newElecResult);
         }
