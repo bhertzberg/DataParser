08ac4f3abe3fe98aa90681a7c57a47c10f84e512
Ben Hertzberg <hertzberg.benjamin@gmail.com>
2019-03-11 01:01:30 -0700

    create county object

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,8 +1,18 @@
+import java.util.List;
+
 public class DataManager {
     private List<State> states;
 
     public class State {
         private String name;
         private List<County> counties;
+    }
+
+    private class County {
+        private String name;
+        private int fips;
+        private Election vote;
+        private Education educ;
+        private Employment employ;
     }
 }
